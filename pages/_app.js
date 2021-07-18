9c90f5ad354920217b5be7810587438933f16d5c
alex <blyakher85@gmail.com>
2021-07-18 20:44:42 +0300

    fix package.json for vercel deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,10 @@
 import {wrapper} from '../store';
 import '../styles/global.css'
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
-
-
-const client = new ApolloClient({
-  uri: '/api/graphql',
-  cache: new InMemoryCache()
-});
 
 const App = function ({ Component, pageProps }) {
   return (
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+    <Component {...pageProps} />
   )
 }
 
